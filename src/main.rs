8fad4dc36eb8c436dac575c7184e0d926a61e51e
Bynawers <bynawers.fratczak@gmail.com>
2023-04-13 00:25:21 +0200

    range reduce

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,64 @@ use sha3::{Sha3_256, Digest};
 
 mod reduction;
 
+const NONCE: u32 = 248820715;
+const ROUND: u32 = 50000;
+
 fn main() {
     let message = "Cryptographie";
-    attack_round(message, 10);
+    attack_round(message, ROUND);
 }
 
 fn attack_round(start: &str, nb_round: u32) {
 
     let mut hash = Sha3_256::digest(start.as_bytes());
 
-    for nounce in 0..nb_round {
-        let password_xor = reduction::reduce_xor(hash.as_slice().try_into().unwrap(), nounce);
-        let password_mod = reduction::reduce_mod(hash.as_slice().try_into().unwrap(), nounce);
-        println!("Tour: {} =>    XOR : {}   |   MOD : {}", nounce, password_xor, password_mod);
-        
+    let mut password_xor: Vec<String> = Vec::new();
+    let mut password_mod: Vec<String> = Vec::new();
+
+    let mut collision_xor: usize = 0;
+    let mut collision_mod: usize = 0;
+
+    for i in 0..nb_round {
+        password_xor.push(reduction::reduce_xor(hash.as_slice().try_into().unwrap(), i+NONCE));
+        password_mod.push(reduction::reduce_mod(hash.as_slice().try_into().unwrap(), i+NONCE));
+        println!("XOR : {}   |   MOD : {}", password_xor[i as usize], password_mod[i as usize]);
+
         hash = Sha3_256::digest(hash);
     }
+
+    println!("sorting...");
+    password_xor.sort();
+    password_mod.sort();
+
+    println!("search collisions...");
+    collision_xor = collision(&password_xor);
+    collision_mod = collision(&password_mod);
+
+    /*
+    for i in password_xor {
+        println!("{}", i);
+    }*/
+    println!(" > {} ROUND :\n   XOR : {} collisions\n   MOD : {} collisions", ROUND, collision_xor, collision_mod);
+
 }
+
+
+fn collision<T: PartialEq>(vec: &[T]) -> usize {
+    let mut count = 0;
+    let len = vec.len();
+
+    // Parcours du vecteur pour compter les éléments égaux consécutifs
+    let mut i = 0;
+    while i < len {
+        let mut j = i + 1;
+        while j < len && vec[j] == vec[i] {
+            count += 1;
+            j += 1;
+        }
+        i = j;
+    }
+
+    count
+}
+
--- src/reduction.rs
+++ src/reduction.rs
@@ -1,5 +1,4 @@
 const SIZE: u8 = 8;
-const SALT_VALUE: u32 = 248715;
 
 const SIGMA_SIZE: u8 = 36;
 const SIGMA: [char; 36] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -14,22 +13,60 @@ fn to_password(bytes: &[u8; 32]) -> String {
     password
 }
 
-pub fn reduce_xor(hash: [u8; 32], nounce: u32) -> String {
+/* FONCTION DE REDUCTION */ 
+
+// a-z0-9 => 8^35 = 2 607 586 401 000 000 possibilitées
+
+// 255 fonctions de réduction différentes (u8)
+// 0 collisions en 500 000 répétitions sur 0,01% de 8^35
+pub fn reduce_xor(hash: [u8; 32], nonce: u32) -> String {
     let mut reduce: [u8; 32] = [0; 32];
 
     for index in 0..32 {
-        reduce[index] = hash[index] ^ (nounce + SALT_VALUE) as u8;
+        reduce[index] = hash[index] ^ nonce as u8;
     }
 
     let password = to_password(&reduce);
     password
 }
 
-pub fn reduce_mod(hash: [u8; 32], nounce: u32) -> String {
+// 4 294 967 295 fonctions de réduction différentes (u32)
+// 0 collisions en 500 000 répétitions sur 0,01% de 8^35
+pub fn reduce_mod(hash: [u8; 32], nonce: u32) -> String {
     let mut reduce: [u8; 32] = [0; 32];
 
+    for i in 0..8 {
+        let hash_slice = &hash[(i * 4)..((i+1) * 4)];
+        let hash_value = u32::from_le_bytes([hash_slice[0], hash_slice[1], hash_slice[2], hash_slice[3]]);
+
+        let hash_reduce = hash_value % nonce;
+        let output = hash_reduce.to_le_bytes();
+
+        reduce[(i * 4)..((i+1) * 4)].copy_from_slice(&output);
+    }
+
+    let password = to_password(&reduce);
+    password
+}
+
+
+
+
+
+
+
+// 255 fonctions de réduction différentes (u8)
+// 1,2% de collisions en moyenne
+pub fn _reduce_mod(hash: [u8; 32], nonce: u32) -> String {
+    let mut reduce: [u8; 32] = [0; 32];
+
+    let mut nonce = nonce;
+
     for index in 0..32 {
-        reduce[index] = hash[index] % (nounce + SALT_VALUE) as u8;
+        if nonce as u8 == 0 {
+            nonce = nonce + 1;
+        }
+        reduce[index] = hash[index] % (nonce as u8);
     }
 
     let password = to_password(&reduce);
