19e7e10830eef0fd52ba81efdc50e630d118408e
uvsq21916099 <thomas.joly@ens.uvsq.fr>
2023-04-21 12:49:22 +0200

    inter

--- src/attack.rs
+++ src/attack.rs
@@ -1,59 +1,68 @@
-use crate::sha3;
-
-use crate::test;
+use sha3::{Sha3_256, Digest};
 
 use crate::reduction;
-
-const NONCE: u32 = 248820715;
-
-const NB_PASSWORD: u32 = 36;
-const NB_NODE: u32 = 4;
-
-#[derive(Debug)]
-#[derive(Clone)]
-pub struct Node {
-    pub start: String,
-    pub end: String,
-}
+use crate::rainbow_table;
+use crate::constants;
 
 pub fn execution() {
-    let message = "a";
-    
+    let message = "Cryptographie";
+    let flag = "psg";
 
-    let mut rainbow_table: Vec<Node> = Vec::new();
+    let mut rainbow_table: Vec<rainbow_table::Node> = Vec::new();
 
-    generate_table(&mut rainbow_table, message, NB_NODE, NB_PASSWORD);
-
-    print_table(&rainbow_table);
-    test::test(&rainbow_table);
+    rainbow_table::generate_table(&mut rainbow_table, message, constants::NB_NODE, constants::NB_PASSWORD);
+    //print_table(&rainbow_table);
+    println!("\nAttack réussi ? {}", search_password(&mut rainbow_table, flag, constants::NB_NODE, constants::NB_PASSWORD));
 }
 
-fn generate_table(rainbow_table: &mut Vec<Node>, start: &str, nb_node: u32, nb_password: u32) {
-    let mut hash = sha3::sha3(start);
-    let mut reduce;
-    let mut node = Node { 
-        start: String::from(""), 
-        end: String::from("") 
-    };
 
-    for i in 0..nb_password {
-        println!("tour : {}",i);
-        for j in 0..nb_node {
-            reduce = reduction::reduce_xor(hash.as_slice().try_into().unwrap(), i+NONCE);
-            hash = sha3::sha3(&reduce.clone());
+
+
+fn search_password(rainbow_table: &mut Vec<rainbow_table::Node>, flag: &str, nb_node: u32, nb_password: u32) -> bool {
+
+    let hash_flag = Sha3_256::digest(flag.as_bytes());
+    let mut reduce = reduction::reduce_xor(hash_flag.as_slice().try_into().unwrap(), constants::NONCE);
+
+    println!("\n> nb_node = {} :", nb_node);
+    println!("> nb_password = {} :", nb_password);
+    println!("> hash_flag = {} :", reduce);
+
+    for i in 0..nb_node {
+        println!("\n> node {} :", i);
+
+        for j in nb_node-(i+1)..nb_node {
             
-            if j == 0 {
-                node.start = reduce.clone();
-            } else if j+1 == nb_node {
-                node.end = reduce.clone();
+            let mut tmp = Sha3_256::digest(reduce.clone());
+            reduce = reduction::reduce_xor(tmp.as_slice().try_into().unwrap(), j+constants::NONCE);
+
+            if j+1 == nb_node {
+                print!("{} (R{})", reduce, j);
+            }
+            else {
+                print!("{} (R{}) => ", reduce, j);
             }
         }
-        rainbow_table.push(node.clone());
+
+        println!("\nsearch {}", reduce);
+        if compare_end(rainbow_table, reduce.clone(), nb_password) {
+            return true;
+        }
     }
-    println!(" > {} ROUND :\n", NB_NODE);
+    return false;
 }
 
-fn print_table(rainbow_table: &Vec<Node>) {
+fn compare_end(rainbow_table: &mut Vec<rainbow_table::Node>, value: String, nb_password: u32) -> bool {
+
+    for i in 0..nb_password {
+        if rainbow_table[i as usize].end == value {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn print_table(rainbow_table: &Vec<rainbow_table::Node>) {
 
     for element in rainbow_table {
         println!("{:?} \n", element);
