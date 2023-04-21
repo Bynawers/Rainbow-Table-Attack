dca9fda742eb8408aab40d535cba04aa79b3f15b
uvsq21916099 <thomas.joly@ens.uvsq.fr>
2023-04-21 12:50:47 +0200

    inter2

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod reduction;
 pub mod performance;
 pub mod sha3;
 pub mod test;
+pub mod constants;
--- target/debug/deps/rainbow_table-6d17b6cbf8ee1f19.d
+++ target/debug/deps/rainbow_table-6d17b6cbf8ee1f19.d
@@ -1,6 +1,6 @@
-/home/ubuntu/Documents/GitHub/Rainbow-Table-Attack/target/debug/deps/rainbow_table-6d17b6cbf8ee1f19.rmeta: src/lib.rs src/attack.rs src/reduction.rs src/performance.rs src/sha3.rs src/test.rs
+/home/ubuntu/Documents/GitHub/Rainbow-Table-Attack/target/debug/deps/rainbow_table-6d17b6cbf8ee1f19.rmeta: src/lib.rs src/attack.rs src/reduction.rs src/performance.rs src/sha3.rs src/test.rs src/constants.rs
 
-/home/ubuntu/Documents/GitHub/Rainbow-Table-Attack/target/debug/deps/rainbow_table-6d17b6cbf8ee1f19.d: src/lib.rs src/attack.rs src/reduction.rs src/performance.rs src/sha3.rs src/test.rs
+/home/ubuntu/Documents/GitHub/Rainbow-Table-Attack/target/debug/deps/rainbow_table-6d17b6cbf8ee1f19.d: src/lib.rs src/attack.rs src/reduction.rs src/performance.rs src/sha3.rs src/test.rs src/constants.rs
 
 src/lib.rs:
 src/attack.rs:
@@ -8,3 +8,4 @@ src/reduction.rs:
 src/performance.rs:
 src/sha3.rs:
 src/test.rs:
+src/constants.rs:
