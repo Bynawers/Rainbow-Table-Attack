3f3f217af7d5616e4dbf68c5070a656752d2f8c9
FRATCZAK Théo <Bynawers.fratczak@gmail.com>
2023-05-03 11:59:30 +0200

    fix bug

--- src/constants.rs
+++ src/constants.rs
@@ -16,10 +16,9 @@ pub const SIGMA_SIZE: u8 = 26;
 pub const SIGMA: [char; SIGMA_SIZE as usize] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 */
 
-pub const SIGMA: [char; SIGMA_SIZE as usize] = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '&', '&', '&',
-'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+pub const SIGMA: [char; SIGMA_SIZE as usize] = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-pub const SIGMA_SIZE: u8 = 65;
+pub const SIGMA_SIZE: u8 = 36;
 
 // Debug
 pub const DEBUG: bool = false;
--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,21 @@
+use crate::{
+    sha3::sha3,
+    reduction::reduction,
+    rainbow_table::Node,
+    constants::*
+};
+
+use colored::*;
+
 #[cfg(test)]
 mod tests {
     use crate::sha3::sha3;
+    use std::fs::File;
+    use std::io::Read;
+    use serde_json::{from_str, Result};
+    use crate::rainbow_table::Node;
+    use crate::test::*;
+    use crate::constants;
 
     #[test]
     fn test_sha3_1() {
@@ -15,4 +30,145 @@ mod tests {
         let res = sha3("crypto");
         assert_eq!(res,hash_crypto)
     }
+
+    #[test]
+    fn test_red_1() {
+        let mut x = sha3("aa");
+        for i in 0..2 {
+            x[i] = x[i] ^ constants::NONCE as u8;
+        }
+        let mut password: String = String::from("");
+
+        for i in 0..2 {
+            password.push(constants::SIGMA[((x[i as usize]) % constants::SIGMA_SIZE) as usize]);
+        }
+        assert_eq!("yw", password)
+    }
+
+    #[test]
+    fn test_attack() {
+        let file = File::open(format!("./data/test/RainbowTable_Test.json"));
+
+        let mut contents = String::new();
+        file.unwrap().read_to_string(&mut contents);
+
+        let nodes: Vec<Node> = from_str(&contents).unwrap();
+        let rainbow_table: Result<Vec<Node>> = Ok(nodes);
+        
+        let res = execution_test(&mut rainbow_table.unwrap(), "ab");
+        assert_eq!(res, true)
+    }   
+}
+
+
+pub fn execution_test(rainbow_table: &mut Vec<Node>, flag: &str) -> bool {
+
+    let mut position_flag;
+
+    let mut tmp: [u8; 32];
+    
+    let hash_flag = sha3(flag);
+    let mut reduce: String = String::from(flag);
+
+    println!("On cherche : {}",flag.yellow());
+
+    for i in 0..50 {
+
+        if DEBUG { println!("{}","\n> Attack Node.. ".yellow()); }
+
+
+        for j in 50-(i+1)..50 {
+
+            if j == 50-(i+1) {
+                tmp = hash_flag;
+                reduce = String::from(flag);
+            }
+            else {
+                tmp = sha3(&reduce.clone());
+            }
+        
+            if DEBUG {
+                if j+1 == NB_NODE {
+                    print!("{} (R{}) => ", reduce, j);
+                }
+                else {
+                    print!("{} (R{}) => ", reduce, j);
+                }
+            }
+
+            reduce = reduction(tmp, j+NONCE);
+            
+        }
+        if DEBUG { print!("{}  ", reduce.red()); }
+
+        if DEBUG { println!("search {}", reduce); }
+
+        position_flag = compare_end_test(rainbow_table, reduce.clone());
+        if position_flag != 101 {
+            if reverse_test(rainbow_table, hash_flag, position_flag) {
+            println!("{} == {} ce truc est {}",reduce,rainbow_table[position_flag as usize].end,reduce==rainbow_table[position_flag as usize].end); 
+                return true;
+            }
+            else {
+                continue;
+            }
+            
+        } 
+        else {
+            if DEBUG { println!("{}", "not find !".red()); }
+        }
+    }
+    println!("{}", "FLAG not found".red());
+    return false;
+}
+
+/*
+*   Compare la value d'entrée à toute les valeurs "end" de la rainbowtable passé en entrée
+*/
+fn compare_end_test(rainbow_table: &mut Vec<Node>, value: String) -> u32 {
+
+    for i in 0..101 {
+        if rainbow_table[i as usize].end == value {
+            if DEBUG {
+                print!("{}", "find !".green());
+                println!(" {} position {}", value, i);
+            }
+            return i;
+        }
+    }
+    return 101;
+}
+
+fn reverse_test(rainbow_table: &mut Vec<Node>, hash_flag: [u8; 32], position_flag: u32) -> bool {
+    
+    if DEBUG {
+        println!("{}", "> Recrate node..".yellow());
+        println!("Position {} : ( first : {}, end: {} )", position_flag, rainbow_table[position_flag as usize].start, rainbow_table[position_flag as usize].end);
+    }
+
+    let mut tmp: [u8; 32];
+    let mut reduce: String = String::from("");
+
+    let mut trouve = false;
+
+    for i in 0..50 {
+
+        if i == 0 {
+            tmp = sha3(&rainbow_table[(position_flag)as usize].start);
+        }
+        else {
+            tmp = sha3(&reduce.clone());
+        }
+
+        if DEBUG { print!("{} (R{}) => ", reduce,i); }
+
+        if tmp == hash_flag {
+            println!("{}", "FLAG found !".green());
+            return true;
+        }
+
+        reduce = reduction(tmp, i+1+NONCE);
+    }
+    if DEBUG && !trouve { println!("{}", "FLAG not found".red()); }
+    return trouve;
 }
